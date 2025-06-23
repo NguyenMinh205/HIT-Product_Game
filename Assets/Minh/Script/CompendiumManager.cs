61dcd48ae8e9bee2d6410cae3a0d35ea53992040
NguyenMinh205 <nqm1312@gmail.com>
2025-06-23 22:42:43 +0700

    Create Base for Compendium Screen
    Add scripts for controlling this screen and create some ScriptableObject for items

---
+++ Assets/Minh/Script/CompendiumManager.cs
@@ -0,0 +1,115 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using static UnityEditor.Progress;
+using TMPro;
+
+public enum CompendiumType
+{
+    Item,
+    Perk
+}
+
+
+public class CompendiumManager : Singleton<CompendiumManager>
+{
+    [Header("Prefab Setting")]
+    [SerializeField] private GameObject compendiumPrefab;
+    [SerializeField] private Transform contentParent;
+
+    [Space]
+    [Header("Show Detail Setting")]
+    [SerializeField] private Sprite itemBG;
+    [SerializeField] private Sprite perkBG;
+    [SerializeField] private Image detailBG;
+    [SerializeField] private Image detailIcon;
+    [SerializeField] private TextMeshProUGUI detailName;
+    [SerializeField] private TextMeshProUGUI detailDescription;
+
+    [Space]
+    [Header("Control")]
+    [SerializeField] private Button leftButton;
+    [SerializeField] private Button rightButton;
+    [SerializeField] private TextMeshProUGUI currentTypeTxt;
+
+    private List<CompendiumType> compendiumTypes;
+    private int currentIndex = 0;
+
+    void Start()
+    {
+        compendiumTypes = new List<CompendiumType>((CompendiumType[])System.Enum.GetValues(typeof(CompendiumType)));
+
+        leftButton.onClick.AddListener(PreviousType);
+        rightButton.onClick.AddListener(NextType);
+
+        LoadCompendium();
+    }
+
+    void LoadCompendium()
+    {
+        foreach (Transform child in contentParent)
+        {
+            PoolingManager.Despawn(child.gameObject);
+        }
+
+        CompendiumType currentType = compendiumTypes[currentIndex];
+        currentTypeTxt.text = currentType.ToString();
+
+
+        if (currentType == CompendiumType.Item)
+        {
+            detailBG.sprite = itemBG;
+            ItemBase[] items = Resources.LoadAll<ItemBase>("ItemSO");
+
+            foreach (ItemBase item in items)
+            {
+                GameObject itemGO = PoolingManager.Spawn(compendiumPrefab, this.transform.position, Quaternion.identity, contentParent);
+                CompendiumItemUI itemUI = itemGO.GetComponent<CompendiumItemUI>();
+                itemUI.Setup(item);
+            }
+        }
+        else if (currentType == CompendiumType.Perk)
+        {
+            detailBG.sprite = perkBG;
+            PerkBase[] perks = Resources.LoadAll<PerkBase>("PerkSO");
+
+            foreach (PerkBase perk in perks)
+            {
+                GameObject perkGO = PoolingManager.Spawn(compendiumPrefab, this.transform.position, Quaternion.identity, contentParent);
+                CompendiumItemUI perkUI = perkGO.GetComponent<CompendiumItemUI>();
+                perkUI.Setup(perk);
+            }
+        }
+    }
+
+    public void ShowDetail(ScriptableObject data)
+    {
+        detailIcon.gameObject.SetActive(true);
+        if (data is ItemBase item)
+        {
+            detailIcon.sprite = item.icon;
+            detailName.text = item.itemName;
+            detailDescription.text = item.description;
+        }
+        else if (data is PerkBase perk)
+        {
+            detailIcon.sprite = perk.icon;
+            detailName.text = perk.perkName;
+            detailDescription.text = perk.description;
+        }
+    }
+
+    public void PreviousType()
+    {
+        currentIndex--;
+        if (currentIndex < 0) currentIndex = compendiumTypes.Count - 1;
+        LoadCompendium();
+    }
+
+    public void NextType()
+    {
+        currentIndex++;
+        if (currentIndex >= compendiumTypes.Count) currentIndex = 0;
+        LoadCompendium();
+    }
+}
---
+++ Assets/Minh/Script/PerkBase.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Perk", menuName = "Inventory/Perk")]
+public class PerkBase : ScriptableObject
+{
+    public string id;
+    public string perkName;
+    public Sprite icon;
+    [TextArea(1, 10)]
+    public string description;
+}
